042e0448d1a65bfd25ce4bfc4c71a3999518a72e
Richard <richard.verheyen@gmail.com>
2022-09-09 15:33:41 +1000

    store the three module outside of node_modules

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import * as THREE from "./../node_modules/three/build/three.module.js";
+import * as THREE from "./three.module.js";
 // import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 const canvas = document.getElementById('hero');
