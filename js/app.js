da8481cf07ad2726bf48c3cf435f7b86592ee9bf
Richard <richard.verheyen@gmail.com>
2022-09-09 16:48:36 +1000

    render sphere and background

--- js/app.js
+++ js/app.js
@@ -3,47 +3,36 @@ import * as THREE from "./three.module.js";
 
 const canvas = document.getElementById('hero');
 
+
+
 // Scene
 const scene = new THREE.Scene();
 
-/**
- * Uncomment the following to add fog to the back of the 
- * scene (at 3 units of distance from the center)
- */
-const fog = new THREE.Fog("#000000", 1, 1000);
-scene.fog = fog;
-
-// Objects
-// const geometry = new THREE.PlaneGeometry(1, 2, 24, 24);
-// const material = new THREE.MeshStandardMaterial({
-//     map: gridTexture,
-//     displacementMap: terrainTexture,
-//     displacementScale: 0.4,
-// });
-
-// const plane2 = new THREE.Mesh(geometry, material);
-// plane2.rotation.x = -Math.PI * 0.5;
-// plane2.position.y = 0.0;
-// plane2.position.z = -1.85; // 0.15 - 2 (the length of the first plane)
-
-// scene.add(plane2);
-
-const geometry = new THREE.SphereGeometry( 1000, 20, 20, Math.PI, Math.PI / 6, Math.PI / 3, Math.PI / 3);
-const material = new THREE.MeshBasicMaterial( { color: 0x00ffff, wireframe: true } );
+// https://threejs.org/docs/#api/en/geometries/SphereGeometry
+const geometry = new THREE.SphereGeometry( 10000, 40, 40, Math.PI, Math.PI / 6, Math.PI * 5 / 12, Math.PI * 2 / 12);
+// const material = new THREE.MeshBasicMaterial( { color: 0x5D9EB8, wireframe: false } );
+const material = new THREE.MeshStandardMaterial( { 
+  color: 0x5D9EB8, 
+  emissive: 0x5D9EB8, 
+  wireframe: false,
+});
 const sphere = new THREE.Mesh( geometry, material );
 const sphere2 = new THREE.Mesh( geometry, material );
 
 sphere.rotation.z = Math.PI * 0.5;
 sphere2.rotation.z = Math.PI * 0.5;
-sphere.position.y = -1000;
-sphere2.position.y = -1000;
+sphere.position.y = -10000;
+sphere2.position.y = -10000;
 scene.add( sphere );
 scene.add( sphere2 );
 
 // Light
 // Ambient Light
-const ambientLight = new THREE.AmbientLight("#ffffff", 10);
+const ambientLight = new THREE.AmbientLight("#ffffff", 0.2);
 scene.add(ambientLight);
+
+const fog = new THREE.Fog("#C9E9E8", 1, 200);
+scene.fog = fog;
 
 // Sizes
 const sizes = {
@@ -61,7 +50,7 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.x = -100;
 camera.position.y = 50;
 camera.position.z = 200;
-camera.lookAt(0, 0, 0);
+camera.lookAt(0, 50, 0);
 
 // Controls
 // const controls = new OrbitControls(camera, canvas);
@@ -70,6 +59,7 @@ camera.lookAt(0, 0, 0);
 // Renderer
 const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
+  alpha: true
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
@@ -90,7 +80,6 @@ window.addEventListener("resize", () => {
 });
 
 const clock = new THREE.Clock();
-console.log({sphere});
 // Animate
 const tick = () => {
     const elapsedTime = clock.getElapsedTime();
